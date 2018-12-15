fe2bf06b4f7b026fe40cde8cdf8eed2fd230d321
Isu <itzuchen5027@gmail.com>
2018-12-16 03:03:09 +0800

    add data-table

--- src/views/PracticeAjax.vue
+++ src/views/PracticeAjax.vue
@@ -1,25 +1,44 @@
 <template>
   <v-container fluid>
     <v-layout row wrap justify-center>
-      <v-flex xs12 class="mb-3">
+      <v-flex xs12 class="mb-4">
         <v-btn to="/" color="black" flat><v-icon left>arrow_left</v-icon>Back to prev page</v-btn>
       </v-flex>
       <v-flex xs12>
-        <v-btn></v-btn>
+        <v-btn disabled="">table</v-btn>
+        <v-btn>chart</v-btn>
       </v-flex>
-      <v-flex xs12>
+      <!-- data-table -->
+      <v-flex xs12 class="pa-2">
+        <v-data-table :headers="headers" :items="characters" hide-actions class="elevation-1">
+          <template slot="items" slot-scope="props">
+            <td>{{ props.item.name }}</td>
+            <td>{{ props.item.height }}</td>
+            <td>{{ props.item.mass }}</td>
+          </template>
+        </v-data-table>
       </v-flex>
     </v-layout>
   </v-container>
 </template>
 <script>
-// @ is an alias to /src
 
 export default {
   components: {
 
   },
   data: () => ({
+    headers: [
+      {
+        text: 'Character Name',
+        align: 'left',
+        sortable: false,
+        value: 'name'
+      },
+      { text: 'Height', value: 'height' },
+      { text: 'Mass', value: 'mass' }
+    ],
+    characters: []
   }),
   mounted() {
     this.$store.commit('SetPageName', { name: this.$route.name })
@@ -47,9 +66,10 @@ export default {
       this.$store.commit('LoadingCircular', { isLoading: true })
       this.$http.get(`https://swapi.co/api/people?page=${ page }`)
         .then( res => {
-          console.log(res)
+          this.characters = res.body.results
         })
         .catch( err => {
+          // 待做處理
           console.log(err)
         })
         .finally(() => {
@@ -61,7 +81,4 @@ export default {
 }
 </script>
 <style scoped>
- * {
-   outline: 1px solid red;
- }
 </style>
